96e55d8f7d540a28bb610e58bbc3eca13dd2fc88
Princewill Usoh <usohps@gmail.com>
2022-12-10 12:17:41 +0100

    second commit

--- src/Hero.js
+++ src/Hero.js
@@ -1,5 +1,4 @@
 import React from "react";
-import ReactTyped from "react-typed";
 function Hero() {
   return (
     <div>
@@ -7,8 +6,7 @@ function Hero() {
         <p className="text-[#87ebcb] capitalize ">growing with data analytics</p>
         <h1 className="text-4xl md:text-6xl font-bold ">Grow with data</h1>
         <div>
-            <p className="font-semibold my-4">Fast, Flexible Financing for  
-            <ReactTyped strings={["BTB","BTC","SASS"]}typeSpeed={120} loop={Infinity} className="p-2" /> </p>
+            <p className="font-semibold my-4">Fast, Flexible Financing for "BTB","BTC","SASS"</p>  
         </div>
         <p className="text-gray-500 md:text-2xl text-center font-semibold">Monitor your data analytics to increase revenue for BTB, BTC, & SASS platforms</p>
         <button className="bg-[#87ebcb] w-[200px] text-black p-2 rounded-md font-bold hover:bg-slate-500 my-4  ease-in-out duration-500 hover:text-white">Get Started</button>
